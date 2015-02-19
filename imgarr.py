8caa5b00f6d6e37bf6059ffdce0902c59a30fae3
indosandi <sandiwibowo@Sandis-MacBook-Air.local>
2015-02-19 03:50:05 -0600

    bisa deh

--- imgarr.py
+++ imgarr.py
@@ -4,30 +4,49 @@ from scipy import misc
 import numpy as np
 class imgarr:
 
+    #init
     def __init__(self):
         self.listInput=[]
         self.nparrayimage=np.array([],dtype=np.float64)
-    def appinput(self,myin):
+
+    #add string to listInput
+    def appInput(self,myin):
         self.listInput.append(myin)
 
+    #fill up listInput with begin end string
+    def fillInput(self,begin,end,total):
+        for i in range(1,total+1):
+            strtemp=begin+str(i)+end
+            self.appInput(strtemp)
+
+    #average from RGB to grayscale
     def rgbToG(self,arr):
-        return (arr[:,:,0]+arr[:,:,1]+arr[:,:,0])/3
+        return (arr[:,:,0]+arr[:,:,1]+arr[:,:,2])/3
+
+    #replace array list input
     def repinput(self,myin):
         self.listInput=myin
+    
+    #image to 1 D array
     def imgToArr1D(self):
         nindex=0;
         img=misc.imread(self.listInput[0])
         xdim=img.shape[0]
         ydim=img.shape[1]
-        self.nparrayimage=np.zeros((xdim*ydim,len(self.listInput)))
+        self.nparrayimage=np.zeros((len(self.listInput),xdim*ydim))
         for value in self.listInput:
             img=misc.imread(value)
-            imgGrey=self.rgbToG(img)
+            if len(img.shape)>2:
+                imgGrey=self.rgbToG(img)
+            else:
+                imgGrey=img
             totalDim=xdim*ydim
             imgGrey1D=np.reshape(imgGrey,totalDim)
-            self.nparrayimage[0:xdim*ydim,nindex]=imgGrey1D[:]
+            self.nparrayimage[nindex,0:xdim*ydim]=imgGrey1D[:]
             nindex+=1
-def imgToArr(self):
+
+    #image to 2D array
+    def imgToArr(self):
         nindex=0;
         img=misc.imread(self.listInput[0])
         xdim=img.shape[0]
--- main.py
+++ main.py
@@ -1,6 +1,13 @@
 import pickle
-from imgarr import imgarr
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib.cm as cm  # 
+#from imgarr import imgarr
 #myarr=imgarr()
+#noimage=6283
+#myarr.fillInput("trainResized/",".bmp",noimage)
+#for i in myarr.listInput:
+    #print(i)
 #a=["img/Lenna.png","Lenna.png"]
 #myarr.repinput(a)
 #myarr.imgToArr1D()
@@ -10,3 +17,26 @@ from imgarr import imgarr
 with open('mydata.obj','rb') as input:
     myarr=pickle.load(input)
     print(myarr.nparrayimage.shape)
+test=np.dot(myarr.nparrayimage.T,myarr.nparrayimage)
+print(test.shape)
+u,s,v=np.linalg.svd(test,full_matrices=True)
+print(u.shape)
+noeigvec=250;
+print(np.sum(s[0:noeigvec])/np.sum(s))
+ucut=np.delete(u,range(noeigvec,u.shape[1]),1)
+print(ucut.shape)
+
+justno=147
+newdata=np.dot(myarr.nparrayimage[justno,:],ucut)
+print(newdata.shape)
+xreduce=np.dot(newdata,ucut.T)
+print(xreduce.shape)
+plt.figure(1)
+plt.imshow(np.reshape(xreduce,(20,20)),cmap=cm.Greys_r)
+plt.figure(2)
+plt.imshow(np.reshape(myarr.nparrayimage[justno,:],(20,20)),cmap=cm.Greys_r)
+plt.show()
+
+#imgcov=np.cov(myarr.nparrayimage.T,myarr.nparrayimage)
+#print(imgcov.shape)
+

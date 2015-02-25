e04946ed2c3081b2ed317c9da913b2200c0343b1
indosandi <sandiwibowo@Sandis-Air.wifi.uwm.edu>
2015-02-25 15:22:25 -0600

    delete file

--- mlearn.py
+++
@@ -1,8 +0,0 @@
-import numpy as np
-from sklearn import cross_validation
-from sklearn import ensemble
-class mlearn:
-
-    #init
-    def init(self):
-        self.objml=null
--- temp.py
+++
@@ -1,9 +0,0 @@
-from streamobj import streamobj
-from csvkag import csvkag
-csvT=csvkag()
-csvT.addInput('trainLabels.csv')
-csvT.genUniqueId()
-print(csvT.npcsvid.shape)
-#print(csvT.npcsvid[6282])
-#print(len(csvT.csvlist))
-streamobj.saveMe(csvT,"trainLabel.obj")
